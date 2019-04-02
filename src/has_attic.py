de6c859fe07a60b06eaaf25dfbdb6b9ff5a64804
twoody <tanner.l.woody@gmail.com>
2019-04-02 12:40:13 -0700

    Moved hasAttic out of test_attic.py;
    Renamed hasAttic to has_attic;

---
+++ src/has_attic.py
@@ -0,0 +1,9 @@
+from pytils.config import *
+def has_attic():
+	''' True if ~/.attic/; Else False'''
+	if os.path.isdir(ATTIC):
+		return True
+	if os.path.isfile(ATTIC):
+		if FLAGS['isQuite'] == False:
+			LOGGER.warning('\n\tpytils expects '+ATTIC+' to be directory;\n\t~/.attic is file instead;')
+	return False
