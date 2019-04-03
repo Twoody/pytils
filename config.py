26d176a7d24c82b5f831fd680f69da2bfe840f17
twoody <tanner.l.woody@gmail.com>
2019-04-02 21:11:41 -0700

    Variable names + get_paths_parent init + upd. iso;
    get_paths_parent put here for lack of dependencies;
    Better var names;
    iso with todo;

--- config.py
+++ config.py
@@ -6,16 +6,20 @@ import logging
 from logging.config import fileConfig
 from datetime import datetime
 from shutil import copyfile
+from glob import glob
 
-HOME = os.path.expanduser("~")
-ATTIC = HOME + '/.attic'
+THIS_FILE=os.path.realpath( __file__ )
+HOME=os.path.expanduser("~")
+ATTIC=HOME + '/.attic'
 #sys.path.append( HOME + '/utils/python/' )
 
 #Logger to log to path defined in ~/.pylog/config.ini
-fileConfig( HOME + '/.pylog/config.ini' )
-FILE=os.path.realpath( __file__ )
-LOGGER=logging.getLogger(FILE)
-
+PYLOG=HOME + '/.pylog' 
+PYLOG_CONFIG=PYLOG + '/config.ini'
+PYLOG_LOGS=PYLOG + '/logs'
+fileConfig(PYLOG_CONFIG)
+LOGGER=logging.getLogger(THIS_FILE)
+HAS_PYLOGS=True if os.path.isfile(PYLOG_CONFIG) else False
 FLAGS={
 	'isQuite':False,
 	'isLogging':True,
@@ -46,4 +50,15 @@ NOS=[
 ]
 
 def iso():
-	return datetime.isoformat(datetime.now())
+	#TDOD: Arguments for just YYYY-M-D or HH:MM
+	#		Could just have an hasYear, hasMonth, ...
+	ret = datetime.isoformat(datetime.now())
+	return ret
+def get_paths_parents(s):
+	''' Get each path of source '''
+	ret = []
+	while s != "/":
+		ret.append(s)
+		s = os.path.dirname(s)
+	ret.reverse()	#Want shortest path first
+	return ret
