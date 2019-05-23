b89711c65faffb1cbb24a3f0a1bf09d752be0d73
twoody <tanner.l.woody@gmail.com>
2019-05-23 14:09:53 -0700

    wcount function;
    Called via .bashrc and wcount;

---
+++ src/word_count/README.txt
@@ -0,0 +1,18 @@
+Author:
+	Tanner.L.Woody@gmail.com
+	2019-05-23
+
+Usage:
+	python3 $PATH/get_word_count.py
+
+Output & Config Defaults:
+ -- -- -- ALL OPTIONS FOUND AT HEADER OF __main__ -- -- --
+ 	cwd       = "/Users/tannerleewoody/Workspace/stackoverflow/not_complete/"
+ 	dest_path = cwd + "macbeth.json"
+ 	iFile     = cwd + 'macbeth.txt'
+ 	show_top  = 20
+
+Purpose:
+	Make a dictionary of all of the present words in a text file
+	Pretty Print the top words with their count;
+	Output dictionary to json file;
---
+++ src/word_count/get_word_count.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+from pytils.config import *
+from pytils.src.get_abs_path import get_abs_path
+import os
+def make_dictionary(txt, args=None):
+ dic = {}
+ with open(txt, 'r') as infile:
+  for line in infile:
+   words = line.split()
+   for word in words:
+    word = word.lower()
+    if word not in dic:
+     dic[word] = 1
+    else:
+     dic[word] += 1
+ # - File closed
+ return dic
+
+def get_word_count(txt, args=None):
+ '''
+   I: A text file;
+   O: A Json file of all the words in that text file
+
+   The outputted json file will be our dictionary;
+ '''
+ cwd       = os.getcwd()
+ show_top  = 20
+ iFile     = cwd + '/' + txt
+ msg       = '\n\tTARGET FILE:\t' + iFile
+ if not os.path.isfile(txt):
+  msg += "\n\tERROR: INVALID ARG: NOT A FILE"
+  return msg
+ if txt[-4:] != ".txt":
+  msg += "\n\tERROR: CAN ONLY READ FROM TEXT FILES"
+  return msg
+ mac_dic   = make_dictionary(iFile)
+ dest_path = cwd + "/" + txt[:-4]+ ".json"
+ suc       = make_json(mac_dic, dest_path)
+ if suc == False:
+  msg += '\n\tBAD RUN; BAILING'
+ else:
+  msg += '\n\tFOUND FILE'
+  msg += '\n\tSORTING DIC'
+  dSorted = sorted(mac_dic.items(), key=operator.itemgetter(1))
+  dSorted.reverse()
+  msg += '\n\tDONE SORTING DIC'
+  msg += '\n\t\tWORD COUNT:\t' + str(len(mac_dic))
+  msg += '\n\t\t'+str(show_top)+' Top Words:'
+  for i in range(0, show_top):
+   word  = str(dSorted[i][0])
+   count = str(dSorted[i][1])
+   msg += '\n\t\t\t' + str(i+1) + '.\t'
+   msg += '`' + word + '`\tAPPEARS '+count+' TIMES'
+ msg += '\n\tFINISHED\n'
+ return msg
+
+def make_json(dic, dest):
+ import json
+ if type(dic) != dict:
+  return False
+ # TODO: Validate dest
+ with open(dest, 'w') as fp:
+  json.dump(dic, fp)
+ return True
+
+if __name__ == "__main__":
+ #Called directly, should have arguments...
+ import operator
+ fullCmdArguments = sys.argv
+ 
+ # - further arguments
+ argumentList = fullCmdArguments[1:]
+ reqs = []
+ for arg in argumentList:
+  isFlag = re.search(FLAG_RE, arg)
+  if isFlag:
+   if arg in ("-v", "--verbose"):
+    if FLAGS['isQuite'] == False:
+     LOGGER.info("\n\tEnabling verbose mode")
+    FLAGS['isVerbose'] = True
+   elif arg in ("-h", "--help"):
+    #TODO: Make a helper function O.o
+    LOGGER.info("\n\tFor current help please email at Tanner.L.Woody@gmail.com")
+    sys.exit(0)
+   elif arg in ("-q", "--quite"):
+    FLAGS['isQuite'] = True
+    #LOGGER.info("\n\tOutput to stdout disabled;")
+   else:
+    if FLAGS['isQuite'] == False:
+     LOGGER.warning("\n\tArgument not recgonized: %s" %arg)
+  else:
+   reqs.append(arg)
+
+ # - Function Calls
+ if len(reqs) == 0:
+  LOGGER.error('\n\t/pytils/src/get_word_count.py: No argment provided')
+  sys.exit(0)
+ elif len(reqs) == 1:
+  msg = get_word_count(reqs[0])
+  print(msg)
+  LOGGER.info(msg)
+ else: 
+  msg = '\n\t/pytils/src/get_word_count.py: TOO MANY ARGUMENTS:'
+  for arg in reqs:
+   msg += '\n\t\tARG:\t%s'%arg
+  LOGGER.error(msg)
+  sys.exit(0)
